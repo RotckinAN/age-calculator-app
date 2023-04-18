493b192367749916e163878addd5a322adb9c651
Anatolii Rotckin <a.rotskin@gmail.com>
2023-04-18 18:35:44 +0200

    feat: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: ['./src/**/*.{html,js}'],
+   theme: {
+      colors: {
+         purple: 'hsl(259, 100%, 65%)',
+         'light-red': 'hsl(0, 100%, 67%)',
+         white: 'hsl(0, 0%, 100%)',
+         'off-white': 'hsl(0, 0%, 94%)',
+         'light-grey': 'hsl(0, 0%, 86%)',
+         'smokey-grey': 'hsl(0, 1%, 44%)',
+         'off-black': 'hsl(0, 0%, 8%)',
+      },
+      fontFamily: {
+         sans: ['Poppins', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+   },
+   plugins: [],
+};
